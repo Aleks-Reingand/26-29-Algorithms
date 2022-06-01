0bb4ae8c89d99df8520fa2603d9d5c1078d387fb
Aleks R <aleks.r@siltatel.fi>
2022-06-01 10:40:02 +0300

    added tests

---
+++ src/test/java/dynamic_array_test/DynamicArrayTest.java
@@ -0,0 +1,22 @@
+package dynamic_array_test;
+
+import homework.dynamic_array.Dynamic;
+import homework.dynamic_array.DynamicArray;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DynamicArrayTest {
+
+    Dynamic array;
+
+    @BeforeEach
+    void init() {
+        array = new DynamicArray();
+        array.add(1);
+    }
+
+    @Test
+    void dynamicArrayAddElementToEndTest() {
+
+    }
+}
